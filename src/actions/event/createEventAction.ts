2ea7f67fca6fadd86c12504e3b3f13a773da13e1
Tomasz Wirkus <wirkus919@gmail.com>
2025-06-20 18:02:50 +0200

    Additional tz fix

--- src/actions/event/createEventAction.ts
+++ src/actions/event/createEventAction.ts
@@ -82,6 +82,7 @@ export async function createEventAction(data: SubmissionFullSchema) {
   const startDate = dayjs(date)
     .set("hour", dayjs(startTime, TIME_FORMAT).hour())
     .set("minute", dayjs(startTime, TIME_FORMAT).minute())
+    .tz("Europe/Warsaw")
     .toDate();
 
   const endDate = dayjs(startDate).add(duration, "minute").toDate();
